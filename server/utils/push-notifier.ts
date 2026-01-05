d4015ac0ecbb11d5bbe2a2d5b4e780ed9e0d3ff1
ddchris <evilkillerice@gmail.com>
2026-01-05 15:15:16 +0800

    移除推播不必要 groupId 參數

--- server/utils/push-notifier.ts
+++ server/utils/push-notifier.ts
@@ -64,7 +64,7 @@ export const buildPushMessages = (eventData: PushEventData) => {
                 type: 'uri',
                 label: '前往投票',
                 uri: useRuntimeConfig().public.liffId
-                  ? `https://liff.line.me/${useRuntimeConfig().public.liffId}?groupId=${(eventData as any).realGroupId || ''}`
+                  ? `https://liff.line.me/${useRuntimeConfig().public.liffId}`
                   : 'https://pick-day.vercel.app/'
               }
             }
